e6720a747577c12c64b436ce24bb5d4913325e99
Nick Miloscia <nickmiloscia@Nicks-MacBook-Pro.local>
2019-07-02 21:37:55 -0400

    Adding Card Flip Project

---
+++ cardFlip/precache-manifest.9523dd5869cf8cb8ca075f5a247b0a48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98496c3eb07e7c082993f602b3ef23a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4035e3abeddb6eb9fcf",
+    "url": "/static/css/main.7967c3f4.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "e4035e3abeddb6eb9fcf",
+    "url": "/static/js/main.5e58ed72.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
