647f281162815999aac79cd4beb0f1558dcef4fa
Nick Miloscia <nickmiloscia@Nicks-MacBook-Pro.local>
2019-07-02 21:43:03 -0400

    Messed up the build of the card flipper app. Hopefully this URL works now.

---
+++ cardFlip/precache-manifest.ed00cb95b1c6ecd15b4b37b780d51fc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4c8a9c1ac0180e515d3c7dc1299416",
+    "url": "/cardFlip/index.html"
+  },
+  {
+    "revision": "df75d0b1faf1ee79724a",
+    "url": "/cardFlip/static/css/main.7967c3f4.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/cardFlip/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "df75d0b1faf1ee79724a",
+    "url": "/cardFlip/static/js/main.07fd4230.chunk.js"
+  },
+  {
+    "revision": "cda806b8e7fb0cc8efea",
+    "url": "/cardFlip/static/js/runtime~main.cdfc182e.js"
+  }
+]);
--- cardFlip/service-worker.js
+++ cardFlip/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9523dd5869cf8cb8ca075f5a247b0a48.js"
+  "/cardFlip/precache-manifest.ed00cb95b1c6ecd15b4b37b780d51fc7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cardFlip/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
